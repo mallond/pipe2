2d05610e8567314d2bbc846530dea727b279c6bc
david mallon <mallond@gmail.com>
2017-09-14 18:47:52 -0500

    cleanup

--- pipe2.js
+++ pipe2.js
@@ -1,14 +1,15 @@
-module.exports = function (RED) {
-  "use strict";
+// Pipe2 Surchage demo Node
+module.exports = function(RED) {
+  'use strict';
   function Pipe2Node(config) {
     RED.nodes.createNode(this, config);
     var node = this;
-    this.on('input', function (msg) {
-      msg.payload.total = msg.payload.total>0?msg.payload.total:msg.payload.amount;
-      msg.payload.steps.push('Pipe 2 apply Surcharge: '+msg.payload.surcharge+' to: ' +msg.payload.total + ' = ' + msg.payload.total*msg.payload.surcharge);
-      msg.payload.total = msg.payload.total*msg.payload.surcharge;
+    this.on('input', function(msg) {
+      msg.payload.total = msg.payload.total > 0 ? msg.payload.total : msg.payload.amount;
+      msg.payload.steps.push('Pipe 2 apply Surcharge: ' + msg.payload.surcharge + ' to: ' + msg.payload.total + ' = ' + msg.payload.total * msg.payload.surcharge);
+      msg.payload.total = msg.payload.total * msg.payload.surcharge;
       node.send(msg);
     });
   }
-  RED.nodes.registerType("pipe2", Pipe2Node);
+  RED.nodes.registerType('pipe2', Pipe2Node);
 };
